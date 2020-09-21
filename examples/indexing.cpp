dbcd952f7c108d35de282fee9db9dac6c2831643
Aniket Sharma <aniketsharma00411@gmail.com>
2020-09-21 23:15:48 +0530

    Indexing can now be performed

---
+++ examples/indexing.cpp
@@ -0,0 +1,24 @@
+#include <Matrix.hpp>
+/* Example program
+
+Read csv files to get a Matrix object.
+Slice the Matrix object and convert it to double.
+The indexing can then be performed.
+*/
+int main() {
+    Matrix mat = read_csv("./datasets/boston/boston.csv");
+    Matrix sliced_mat = mat.slice(1, mat.row_length(), 0, mat.col_length());
+    sliced_mat.to_double();
+
+    // The value of a particular index can be get as double
+    double val = sliced_mat(1, 2);
+    std::cout << val << std::endl;
+
+    // The value can be assigned to a particular index
+    sliced_mat(1, 2) = 5;
+    std::cout << sliced_mat(1, 2) << std::endl;
+
+    // An index value can also be printed using the view() method
+    sliced_mat.view(1, 2);
+    return 0;
+}
