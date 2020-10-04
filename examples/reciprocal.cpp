7f71f37b02cd741e456c2310b77755db361cf99e
Aniket Sharma <aniketsharma00411@gmail.com>
2020-10-04 15:10:49 +0530

    exp, reciprocal and unary_minus added

---
+++ examples/reciprocal.cpp
@@ -0,0 +1,20 @@
+#include <Matrix.hpp>
+
+/* Example program
+
+Read a csv file and get a Matrix object.
+Slice the Matrix object to remove the rows and columns which cannot be converted to double.
+The sliced Matrix object is then converted to double and then the reciprocal() method is applied to
+it.
+*/
+int main() {
+    Matrix mat = read_csv("./datasets/boston/boston.csv");
+    Matrix sliced_mat = mat.slice(1, mat.row_length(), 0, mat.col_length());
+    sliced_mat.to_double();
+
+    Matrix reciprocal_mat = matrix.reciprocal(sliced_mat);
+
+    reciprocal_mat.print();
+
+    return 0;
+}
---
+++ examples/unary_minus.cpp
@@ -0,0 +1,19 @@
+#include <Matrix.hpp>
+
+/* Example program
+
+Read a csv file and get a Matrix object.
+Slice the Matrix object to remove the rows and columns which cannot be converted to double.
+The sliced Matrix object is then converted to double and then the unary minus operator is applied to
+it.
+*/
+int main() {
+    Matrix mat = read_csv("./datasets/boston/boston.csv");
+    Matrix sliced_mat = mat.slice(1, mat.row_length(), 0, mat.col_length());
+    sliced_mat.to_double();
+    sliced_mat = -sliced_mat;
+
+    sliced_mat.print();
+
+    return 0;
+}
